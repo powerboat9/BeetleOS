0b07c38e09dca53f89349e10a7a143beb948f291
powerboat9 <powerboat9@users.noreply.github.com>
2016-08-03 00:55:39 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,6 +1,34 @@
-function readAPI(f)
+function parseMetadata(f)
+    data = {}
+    local f = fs.open(f, "r")
+    if !f then
+        return false
+    end
+    while true do
+        local line = f.readLine()
+        if !line then
+            break
+        end
+        if line:sub(1, 1) ~= "@" then
+            break
+        end
+        
 
-errAPIS = {}
+function readAPI(f)
+    local data = {}
+    if fs.exists(f .. "/pkg.txt") then
+        local f = fs.open(f .. "/pkg.txt")
+        data = textutils.unserialize(f.readAll())
+        f.close()
+    else
+        return false
+    end
+    if type(data.name) ~= "string" then
+        return false
+    end
+    if type(data.dependenc
+
+local errAPIS = {}
 for _, v in ipairs(fs.list("/.APIS")) do
     if not os.loadAPI("/.APIS/" .. v) then
         errAPIS[#errAPIS + 1] = v
